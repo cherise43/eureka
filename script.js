744a4fe668144741ff947c011af3a0f5ac9d7a7a
Cherise <nashpaiessy@gmail.com>
2023-07-07 16:03:17 +0300

    added html document

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('https://fakestoreapi.com/products')
-  .then(response => response.json())
-  .then(items => renderItems(items))
-  .catch(error => {
-    console.error('Error fetching items:', error);
-  });
-}
-fetchItems()
-function renderItems(items){ 
-  items.forEach(item => {
-    let card = document.createElement('div')
-    card.className="card"
-    card.innerHTML=`
-    <div id="product-image">
-    <img src ="${item.image}">
-    <div>
-    <h3>${item.category}</h3>
-    <p>Price:${item.price}</p>`
-    console.log(card)
 
-    document.querySelector('#items').appendChild(card)
-  })
-}
-
-
-
-   document.addEventListener('click', (event) => {
-      if (event.target.classList.contains('add-to-cart-btn')) {
-           const itemId = event.target.dataset.itemId;
-          addToCart(itemId);
-          } else if (event.target.id === 'checkout-btn') {
-             checkout();
-             }
-             });
-                document.getElementById('comments-form').addEventListener('submit', (event) => {
-                 event.preventDefault();
-                 const commentInput = document.getElementById('comment-input');
-                  const comment = commentInput.value.trim();
-        //     if (comment !== '') {
+  function fetchItems() {
+     fetch('https://fakestoreapi.com/products')
+       .then(response => response.json())
+       .then(items => renderItems(items))
+       .catch(error => {
+         console.error('Error fetching items:', error);
+       });
+    }
+    fetchItems()
+ 
+    function renderItems(items){
+       items.forEach(item => {
+          let card = document.createElement('div')
+          card.className="card"
+          card.innerHTML=`
+          <div id="product-image">
+             <img src ="${item.image}">
+          <div>
+          <h3>${item.category}</h3>
+          <p>Price:${item.price}</p>`
+          console.log(card)
+          document.querySelector('#items').appendChild(card)
+       })
+    }
+  //       document.addEventListener('click', (event) => {
+  //     if (event.target.classList.contains('add-to-cart-btn')) {
+  //       const itemId = event.target.dataset.itemId;
+  //       addToCart(itemId);
+  //     } else if (event.target.id === 'checkout-btn') {
+  //       checkout();
+  //     }
+  //   });
+  
+  //   document.getElementById('comments-form').addEventListener('submit', (event) => {
+  //     event.preventDefault();
+  //     const commentInput = document.getElementById('comment-input');
+  //     const comment = commentInput.value.trim();
+  //     if (comment !== '') {
   //       addComment(comment);
   //       commentInput.value = '';
   //     }
   //   });
+  
+       
+      
+    
   
   //   function createItemElement(item){
   //     const itemElement = document.createElement('div');
@@ -126,9 +130,4 @@ function renderItems(items){
   //     comments.push(comment);
   //     loadComments();
   //   }
-  })        
-  
-       
-      
-    
-  
+  })
