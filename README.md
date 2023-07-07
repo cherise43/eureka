0169020d19104c49021cd20657547f4236b98967
Cherise <nashpaiessy@gmail.com>
2023-07-07 12:54:33 +0300

    completed assignment

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# eureka
+# EUREKA 
+Phase 1,EUREKA ONLINE STORE
+
+# DESCRIPTION
+Creating an application /programs information of certain item,price,location,the quality of an item,a button that informs the cilent of what item to purchase.
+
+# Project SetUp
+* 1.Fork the repository 
+2.Clone this repository `git@github.com:cherise43/eureka.git`
+3.Open code in Visual Studio Code by typing `code.`in your terminal.
+4.Open the html file of the project and click on the live server to launch the program.
+5.Click a button to display the number of items.
+
+
+
+
+# Requirements 
+* Ubuntu
+* Visual Studio Code
+* Moderate Internet Connection
+
+# Author 
+Cathy Maseki
+
+# License
+MIT License
+
+Copyright (c) [2023] [Cathy Maseki]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+
---
+++ script.js
@@ -0,0 +1,132 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  function fetchItems() {
+     fetch('https://fakestoreapi.com/products')
+       .then(response => response.json())
+       .then(items => renderItems(items))
+       .catch(error => {
+         console.error('Error fetching items:', error);
+       });
+    }
+    fetchItems()
+ 
+    function renderItems(items){
+       items.forEach(item => {
+          let card = document.createElement('div')
+          card.className="card"
+          card.innerHTML=`
+          <div id="product-image">
+             <img src ="${item.image}">
+          <div>
+          <h3>${item.category}</h3>`
+          console.log(card)
+          document.querySelector('#items').appendChild(card)
+       })
+    }
+  //       document.addEventListener('click', (event) => {
+  //     if (event.target.classList.contains('add-to-cart-btn')) {
+  //       const itemId = event.target.dataset.itemId;
+  //       addToCart(itemId);
+  //     } else if (event.target.id === 'checkout-btn') {
+  //       checkout();
+  //     }
+  //   });
+  
+  //   document.getElementById('comments-form').addEventListener('submit', (event) => {
+  //     event.preventDefault();
+  //     const commentInput = document.getElementById('comment-input');
+  //     const comment = commentInput.value.trim();
+  //     if (comment !== '') {
+  //       addComment(comment);
+  //       commentInput.value = '';
+  //     }
+  //   });
+  
+       
+      
+    
+  
+  //   function createItemElement(item){
+  //     const itemElement = document.createElement('div');
+  //     itemElement.className = 'item';
+  
+  //     const imageElement = document.createElement('img');
+  //     imageElement.src = item.image;
+  //     imageElement.alt = item.title;
+  //     itemElement.appendChild(imageElement);
+  
+  //     const titleElement = document.createElement('h3');
+  //     titleElement.textContent = item.title;
+  //     itemElement.appendChild(titleElement);
+  
+  //     const priceElement = document.createElement('p');
+  //     priceElement.textContent = `$${item.price}`;
+  //     itemElement.appendChild(priceElement);
+  
+  //     const addToCartButton = document.createElement('button');
+  //     addToCartButton.textContent = 'Add to Cart';
+  //     addToCartButton.className = 'add-to-cart-btn';
+  //     addToCartButton.dataset.itemId = item.id;
+  //     itemElement.appendChild(addToCartButton);
+  
+  //     return itemElement;
+  //   }
+  
+  //   let cart = [];
+  
+  //   function addToCart(itemId) {
+  //     const item = getItemById(itemId);
+  //     if (item) {
+  //       cart.push(item);
+  //       updateCartDisplay();
+  //     }
+  //   }
+  
+  //   function getItemById(itemId) {
+  //     // Retrieve item by ID from the API or a local item list
+  //     // This is a simplified example assuming the item exists
+  //     return cart.find(item => item.id === itemId);
+  //   }
+  
+  //   function updateCartDisplay() {
+  //     const cartItemsList = document.getElementById('cart-items');
+  //     cartItemsList.innerHTML = '';
+  
+  //     cart.forEach(item => {
+  //       const cartItemElement = document.createElement('li');
+  //       cartItemElement.textContent = item.title;
+  //       cartItemsList.appendChild(cartItemElement);
+  //     });
+  //   }
+  
+  //   function checkout() {
+  //     if (cart.length > 0) {
+  //       console.log('Checkout');
+  //       cart = [];
+  //       updateCartDisplay();
+  //     } else {
+  //       console.log('Cart is empty. Add items before checking out.');
+  //     }
+  //   }
+  
+  //   let comments = [];
+  
+  //   function loadComments() {
+  //     const commentsList = document.getElementById('comments-list');
+  //     commentsList.innerHTML = '';
+  
+  //     comments.forEach(comment => {
+  //       const commentElement = document.createElement('li');
+  //       commentElement.className = 'comment';
+  //       const commentText = document.createElement('p');
+  //       commentText.textContent = comment;
+  //       commentElement.appendChild(commentText);
+  //       commentsList.appendChild(commentElement);
+  //     });
+  //   }
+  
+  //   function addComment(comment) {
+  //     comments.push(comment);
+  //     loadComments();
+  //   }
+  })
